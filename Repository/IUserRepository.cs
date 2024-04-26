de8d7b478aec26e6b3385f19c01fa39b81d06c9f
Bande_Meliza_BSIT32E1 <155070275+BandeMeliza-BSIT32E1@users.noreply.github.com>
2024-04-27 01:38:14 +0800

    Add files via upload

---
+++ Repository/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using Domain; // Assuming the User class is defined in the Domain namespace
+
+namespace Repository
+{
+    public interface IUserRepository
+    {
+        User Create(User user);
+        
+        User GetById(int id);
+        
+        User GetByUsername(string username);
+        
+        IEnumerable<User> GetAll();
+    }
+}
---
+++ Service/Services.cs
@@ -0,0 +1,35 @@
+﻿using Repository;
+using Domain;
+
+namespace Service
+{
+    public class Services : IUserService
+    {
+        private readonly IUserRepository _repository;
+
+        public Services(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public User Create(User todo)
+        {
+            return _repository.Create(todo);
+        }
+
+        public User GetById(int id)
+        {
+            return _repository.GetById(id);
+        }
+
+        public User GetByUsername(string username)
+        {
+            return _repository.GetByUsername(username);
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            return _repository.GetAll();
+        }
+    }
+}
